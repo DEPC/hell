564af9c26c0f265b023e2e3aa521c9431f319132
DEPC <pegdavid@gmail.com>
2015-02-09 16:54:35 -0500

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,3 +1,7 @@
 public static void main(String args[]){
-  System.out.println("Hello World!");
+  int array[1,2,3,4,5];
+  int sum = 0;
+  for(int i = 0; i < 4; i++){
+      sum += array[i];
+  }
 }
